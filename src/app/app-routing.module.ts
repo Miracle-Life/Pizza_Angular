2cccefda6cc9aa7f450018721ed95b7ac02c252e
Denis <belovdenis777@gmail.com>
2019-09-09 11:09:55 +0300

    Routing successfully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { BodyComponent } from './body/body.component';
 import { BodyHomeComponent } from './body/body-home/body-home.component';
 import { BodyOrderComponent } from './body/body-order/body-order.component';
 import { BodyAboutUsComponent } from './body/body-about-us/body-about-us.component';
@@ -7,6 +9,7 @@ import { BodyContactComponent } from './body/body-contact/body-contact.component
 
 
 const routes: Routes = [
+  { path: 'Body', component: BodyComponent },
   { path: 'BodyHome', component: BodyHomeComponent },
   { path: 'BodyOrder', component: BodyOrderComponent },
   { path: 'BodyAboutUs', component: BodyAboutUsComponent },
